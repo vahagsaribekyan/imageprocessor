811c767fb3f439dc6713f2b0c6b870bd1afb5a39
vahagsaribekyan <32436397+vahagsaribekyan@users.noreply.github.com>
2019-10-03 11:57:16 +0400

    Small fix

--- index.js
+++ index.js
@@ -29,3 +29,31 @@ const execute = async () => {
     // response = await uploadLocalFile("samples", localFilePath);
 
 execute().then(() => console.log("Done")).catch((e) => console.log(e));
+
+
+
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const Instagram = require('instagram-web-api');
+const instagramClient = new Instagram({username: '', password: ''});
+
+const PORT = 8080;
+
+const handler = async (req, res) => {
+    const user = await instagramClient.getUserByUsername({ username: req.params.username });
+    res.json(user);
+};
+
+let setupOpenRoutes = () => {
+    const router = express.Router();
+    router.get('/users/:username', handler);
+    return router;
+};
+
+const openRoutes = setupOpenRoutes();
+app.use('/', openRoutes);
+
+app.listen(PORT, console.info(`Listening on port: ${PORT}`));
